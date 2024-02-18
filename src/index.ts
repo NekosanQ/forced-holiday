63bae341c32cc721e169b744440cbe69a6c2b79a
NekosanQ <NekosanQ@outlook.com>
2024-02-19 02:02:15 +0900

    コメントアウト追加

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 const prompts = require('prompts');
-const date: string = new Date().toLocaleString('ja-JP', { timeZone: 'Asia/Tokyo' });
+const date: string = new Date().toLocaleString('ja-JP', { timeZone: 'Asia/Tokyo' }); // 現在時刻を取得
 const fs = require("node:fs");
 
 (async () => {
@@ -12,7 +12,7 @@ const fs = require("node:fs");
 
     let day_of_week = response.day === 'y' ? '休日' : '平日';
     console.log(`今日は${day_of_week}です。`);
-    fs.appendFile("logs/history.log", `[${date}] 今日は${day_of_week}です！\n`, (err: Error) => {
+    fs.appendFile("logs/history.log", `[${date}] 今日は${day_of_week}です！\n`, (err: Error) => { // ログファイルに書き込む
         if (err) throw err;
     });
 })();
